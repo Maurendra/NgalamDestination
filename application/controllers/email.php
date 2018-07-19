3e18deb9bfd9bfda4bc11a01d1bb0f6df6d32e07
Maurendra <33946189+Maurendra@users.noreply.github.com>
2018-07-19 22:13:39 +0700

    Update email.php

--- application/controllers/email.php
+++ application/controllers/email.php
@@ -7,15 +7,15 @@ class Email extends CI_Controller {
     'protocol' => 'smtp',  
     'smtp_host' => 'ssl://smtp.googlemail.com',  
     'smtp_port' => 465,  
-    'smtp_user' => 'mrendra25@gmail.com',   
-    'smtp_pass' => 'mrendra25@gmail',   
+    'smtp_user' => 'tryit@gmail.com',   
+    'smtp_pass' => 'tryit',   
     'mailtype' => 'html',   
     'charset' => 'iso-8859-1'  
   );  
    $this->load->library('email', $config);  
    $this->email->set_newline("\r\n");  
    $this->email->from('recodeku@gmail.com', 'Admin Re:Code');   
-   $this->email->to('mrendra25@gmail.com');   
+   $this->email->to('try@gmail.com');   
    $this->email->subject('Percobaan email');   
    $this->email->message('Ini adalah email percobaan untuk Tutorial CodeIgniter: Mengirim Email via Gmail SMTP menggunakan Email Library CodeIgniter @ recodeku.blogspot.com');  
    if ($this->email->send()) {  
